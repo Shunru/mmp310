c31fdf744d247c21ca4418e4162949aa5aea81dd
Shunru <shun.xiao@f1006-09-34181.bmcc.cuny.edu>
2016-02-24 17:51:25 -0500

    add more funtions

---
+++ week4/script.js
@@ -0,0 +1,16 @@
+function foo(){
+    alert("Hello");
+}
+foo();
+console.log(typeof foo);
+myFuction();
+var bar = function(){
+    document.write("world.");
+}
+var();
+console.log(typeof bar);
+var greet = function(name, greeting){
+var text = greeting +","+name;
+document.write(text);
+}
+greet();
