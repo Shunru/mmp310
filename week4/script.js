d5cbf961e3542b6ca1cd65d9813c63e657d4a75d
Shunru <shun.xiao@f1006-09-34181.bmcc.cuny.edu>
2016-02-24 18:33:32 -0500

    add changes in the js

--- week4/script.js
+++ week4/script.js
@@ -12,7 +12,58 @@ console.log(typeof bar);
 var greet = function(name, greeting){
 var text = greeting +","+name;
 document.write(text);
-}
+};
+
 greet();
 greet("Jenny","Hello");
 greet("Jerry ","Hi");
+
+var add = function (n1, n2){
+    console.log(arguments);
+    return n1 + n2;
+
+};
+
+var x = add(2,2);
+var y = add(x, 2);
+var z = add (x, y);
+cocument.write("<p>" +add(x,y)+"</p>");
+
+
+(function(x){
+    alert(x);
+})("Hwllo world!");
+
+
+var assign = function(){
+    var n = 0;
+    var m = 1;
+};
+
+var multiply = function(){
+    return n * m;
+
+};
+assign();
+multiply();
+
+var j;
+var k;
+
+
+var assignValue = function(){
+    j = 5;
+    k = 2;
+
+};
+var divide = function(){
+    return j/ k;
+
+};
+assignValue();
+val l = divide();
+document.write("<p>+l+"</p>");
+
+
+
+
