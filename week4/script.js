78c7d086bca1c7d4047eb6a45ac5d5c4fd339cc0
Shunru <shun.xiao@f1006-09-34181.bmcc.cuny.edu>
2016-02-29 18:29:25 -0500

    add more information on html and javascript

--- week4/script.js
+++ week4/script.js
@@ -1,69 +1,18 @@
-function foo(){
-    alert("Hello");
-}
-foo();
-console.log(typeof foo);
-myFuction();
-var bar = function(){
-    document.write("world.");
-}
-bar();
-console.log(typeof bar);
-var greet = function(name, greeting){
-var text = greeting +","+name;
-document.write(text);
-};
 
-greet();
-greet("Jenny","Hello");
-greet("Jerry ","Hi");
-
-var add = function (n1, n2){
-    console.log(arguments);
-    return n1 + n2;
-
-};
-
-var x = add(2,2);
-var y = add(x, 2);
-var z = add (x, y);
-cocument.write("<p>" +add(x,y)+"</p>");
-
-
-(function(x){
-    alert(x);
-})("Hwllo world!");
-
-
-var assign = function(){
-    var n = 0;
-    var m = 1;
-};
-
-var multiply = function(){
-    return n * m;
-
-};
-assign();
-multiply();
-
-var j;
-var k;
-
-
-var assignValue = function(){
-    j = 5;
-    k = 2;
-
-};
-var divide = function(){
-    return j/ k;
-
-};
-assignValue();
-val l = divide();
-document.write("<p>+l+"</p>");
-
-
+var messages = ["Yes, abosulate",
+                "No, not that I konw it.",
+                "Maybe, it's possible.",
+                "Ask me later.",
+                "As I see it, yes.",
+                "Not today.",
+                "Cant predict now.",
+                "Dont count on it.",
+                "My sources say yes.",
+                "Dont even think about it.",
+                "Very doubtful",
+                "You can rely on it.",
+                "It's certainly.",
+                "Better not tell you now.",
+               ];
 
 
