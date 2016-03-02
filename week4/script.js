9b58a1606f378612ee6eefa84a6dc7825fd565df
shunru <smnthsiu@gmail.com>
2016-03-01 21:46:30 -0500

    made correction

--- week4/script.js
+++ week4/script.js
@@ -1,5 +1,5 @@
 
-var messages = ["Yes, abosulate",
+var answers = ["Yes, abosulately.",
                 "No, not that I konw it.",
                 "Maybe, it's possible.",
                 "Ask me later.",
@@ -8,16 +8,27 @@ var messages = ["Yes, abosulate",
                 "Cant predict now.",
                 "Dont count on it.",
                 "My sources say yes.",
-                "Dont even think about it.",
-                "Very doubtful",
+               "Dont even think about it.",
+               "Very doubtful",
                 "You can rely on it.",
-                "It's certainly.",
+                "I hope so.",
                 "Better not tell you now.",
+                "Most likely.",
                ];
+    result = function(){
+        var randomnumber = Math.floor(Math.random()*15);
+
+   document.getElementById('res').innerHTML ="Answer"+answers[randomnumber];
+
+    }
+
+    $(#bigball).click(){
+      $('.shake').effect("shake",{
+        times:8,
+        direction:"up"
+      },3000);
+var mytimer = window.setTimeout('result()',3000);
+});
 
 
-.circle{
-    background-color:blue;
-
-}
 
