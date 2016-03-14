cf4c08d69d80148c4e1c9c5fc0bc1c5839d0a6ab
Shunru <shun.xiao@f1006-09-34181.bmcc.cuny.edu>
2016-03-14 18:56:48 -0400

    hnklm .,

--- week5/app.js
+++ week5/app.js
@@ -1,38 +1,38 @@
-function submitAnswers(){
-	var total = 6;
-	var score = 0;
+function submitAnswers() {
+    var total = 6;
+    var score = 0;
 
-	//input
-	var question1 = document.forms["quizForm"]["q1"].value;
-	var question2 = document.forms["quizForm"]["q2"].value;
-	var question3 = document.forms["quizForm"]["q3"].value;
-	var question4 = document.forms["quizForm"]["q4"].value;
-	var question5 = document.forms["quizForm"]["q5"].value;
-	var question6 = document.forms["quizForm"]["q6"].value;
+    //input
+    var question1 = document.forms["quizForm"]["q1"].value;
+    var question2 = document.forms["quizForm"]["q2"].value;
+    var question3 = document.forms["quizForm"]["q3"].value;
+    var question4 = document.forms["quizForm"]["q4"].value;
+    var question5 = document.forms["quizForm"]["q5"].value;
+    var question6 = document.forms["quizForm"]["q6"].value;
 
-	//loop
-	for(i = 1; i <= total;i++){
-		if(eval('q'+i) == null || eval('q'+i) == ''){
-			alert('You Missed Question ' +i);
-			return false;
-		}
-	}
+    //loop
+    for (i = 1; i <= total; i++) {
+        if (eval('q' + i) == null || eval('q' + i) == '') {
+            alert('You Missed Question ' + i);
+            return false;
+        }
+    }
 
-	//Set Correct Answers
-	var answers = ["B","C","D","C","D","B"];
+    //Set Correct Answers
+    var answers = ["B", "C", "D", "C", "D", "B"];
 
-	//Check Answers
+    //Check Answers
 
-    for (i =1; i<= total; i++){
-		if(eval('q'+i)== answers[i - 1]){
-			score++;
-		}
-	}
-	//Display Results
-	var results = document.getElementById('results');
-	results.innerHTML = '<p> You Scored <span>'+score+'</span> out of <span>'+total+'</span></p>';
-	document.getElementById('results').style.display = 'block';
-	//alert('You Scored '+ score +' out of ' + total);
+    for (i = 1; i <= total; i++) {
+        if (eval('q' + i) == answers[i - 1]) {
+            score++;
+        }
+    }
+    //Display Results
+    var results = document.getElementById('results');
+    results.innerHTML = '<p> You Scored <span>' + score + '</span> out of <span>' + total + '</span></p>';
+    document.getElementById('results').style.display = 'block';
+    //alert('You Scored '+ score +' out of ' + total);
 
-	return false;
+    return false;
 }
