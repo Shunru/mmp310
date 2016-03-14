d7aeb91287a489308b492bb6952dac147f0ef05c
Shunru <shun.xiao@f1006-09-34181.bmcc.cuny.edu>
2016-03-14 19:12:17 -0400

    hiujklm

--- week6/list.js
+++ week6/list.js
@@ -1,7 +1,6 @@
-function toaddlist(){
-    var task = document.getElementById("task")
-    var text = document.createTextNode(task.value)
-    var newItem = document.createElement("li")
-    newItem.appendChild(text)
-    document.getElementById("toAdd").appendChild(newItem)
-}
+
+    var newTask = document.getElementById("newTask");
+    var addButton = document.getElementByTagName("button");
+    var incompleteTaskHolder= document.getElementById(incomplete-tasks");
+
+    var completedTaskHolder= document.getElementById("completed-tasks");
