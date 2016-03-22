b0fc164085a6f0aecfee63a08a762ee9eaa990e2
shunru <smnthsiu@gmail.com>
2016-03-21 23:41:00 -0400

    islknlgzsdf

--- week7 /gen.js
+++ week7 /gen.js
@@ -1,22 +1,24 @@
 function randomPassword(length){
-    var keyval = document.getElementById("key");
-    var key = keyval.value;
+    var spcval = document.getElementById("specialkey");
+    var spc = spcval.value;
     
     var chars="abcdefghijklmnopqrstuvwxyzABCEEFGHIJKLMNOPFQRSTUVWXYZ0123456789";
     
     if(key === "Yes"){
         chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890{};&*%#$@!/?<>'";
     }
+    
     var pass ="";
     for (var x =0; x <length; x++){
         i = Math.floor(Math.random()*chars.length);
         pass += chars.charAt(i);
         
     }
+    document.getElementById("result").innerHTML = password;
+}
     
     
-    function generate(){
+    function dormSubmit(){
         passform.passbox.value= randomPassword(passform.length.value);
     }
   
-}
