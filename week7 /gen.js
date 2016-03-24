8f92c03994a015124beb5f257d48e699dff7c564
shunru <smnthsiu@gmail.com>
2016-03-23 22:58:43 -0400

    l,me

--- week7 /gen.js
+++ week7 /gen.js
@@ -1,12 +1,42 @@
+
 function makePassword(){
     var password = "";
+<<<<<<< HEAD
     var chars="abcdefghijklmnopqrstuvwxyzABCEEFGHIJKLMNOPFQRSTUVWXYZ0123456789@#$%^&*()+_":";
+=======
+    var chars="abcdefghijklmnopqrstuvwxyzABCEEFGHIJKLMNOPFQRSTUVWXYZ0123456789!@#$%^&*+)}|:";
+>>>>>>> origin/gh-pages
     for (var i =0; i <10; i++){
+
+function randomPassword(length){
+    
+    var spcval = document.getElementById("specialkey");
+    var spc = spcval.value;
+    
+    var chars="abcdefghijklmnopqrstuvwxyzABCEEFGHIJKLMNOPFQRSTUVWXYZ0123456789";
+    
+    if(spc === "Yes"){
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890{};&*%#$@!/?<>'";
+    }
+    
+   var password = "";
+    for (var i =0; i <length; i++){
+
         console.log(i);
         
         var c = Math.floor(Math.random()*chars.length);
-        password += chars.charAt(c)
+        password += chars.charAt(i);
+        
     }
-    document.getElementById("result").innerHTML = password;
   
 }
+
+  function myFunction(){
+      myform.passbox.value = myFunction(myform.length.value);
+  }
+    
+document.getElementById("result").innerHTML = password;
+
+ 
+   
+    
